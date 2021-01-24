b6b5c2ba207e014dcc7b24546cc6bd6c8a9dbf29
7555G <630r63.7555@gmail.com>
2021-01-24 16:55:36 +0200

    Frist commit

---
+++ etc/post-install.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# disable user and system zsys snapshots
+echo "- Disabling ZFS zsys snapshots"
+if modinfo zfs &> /dev/null; then
+    systemctl --user stop zsys-user-savestate.timer
+    systemctl --user disable zsys-user-savestate.timer
+    sudo mv /etc/apt/apt.conf.d/90_zsys_system_autosnapshot \
+        /etc/apt/apt.conf.d/90_zsys_system_autosnapshot.disabled
+fi
+
+# uninstall default wireless driver, and install working one
+echo "- Install working wireless driver and remove default"
+sudo apt purge bcmwl-kernel-source
+sudo apt-get install broadcom-sta-source
+sudo apt-get install broadcom-sta-dkms
+sudo apt-get install broadcom-sta-common
+
+# generate nvidia xorg config file (installed driver)
+if [[ ! -f /etc/X11/xorg.conf ]]; then
+    echo "- Created /etc/X11/xorg.conf"
+    sudo nvidia-xconfig
+fi
+
+# add brightness adjustment option and hide logo on boot
+vendor_name='    VendorName     "NVIDIA Corporation"'
+brightness_option=\
+'\    Option         "RegistryDwords" "EnableBrightnessControl=1"'
+nologo_option='\    Option         "NoLogo"'
+
+xorg_file="/etc/X11/xorg.conf"
+xorg_file="$HOME/afile"
+if (! grep -q "${brightness_option}" "${xorg_file}"); then
+    sed "/${vendor_name}/a\
+    ${brightness_option}\n${nologo_option}"\
+    "${xorg_file}" > ~/xorg.tmp
+fi
+
+sudo mv xorg.tmp /etc/X11/xorg.conf
+
+# install some packages
+sudo apt update
+sudo apt full-upgrade
+sudo apt install tree vim-gtk3 xbacklight
---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash 
+
+# dotfiles path
+DOTFILES=$(\
+    builtin cd "$(\
+    dirname "$(readlink -f "${BASH_SOURCE[0]}")")"\
+    > /dev/null && pwd)
+MACOS_DOTFILES=~/.macos-dotfiles
+
+# prepare folders
+mkdir -vp ~/.vim/undo
+mkdir -vp ~/.vim/spell
+
+# make soft symlinks
+echo "- Symlinking dotfiles (${DOTFILES})"
+"${MACOS_DOTFILES}/bin/ln_dotfiles"    ${DOTFILES}
+ln -sfv ${MACOS_DOTFILES}/inputrc      ~/.inputrc
+ln -sfv ${MACOS_DOTFILES}/bash_aliases ~/.bash_aliases
+ln -sfv ${MACOS_DOTFILES}/vim/*        ~/.vim
