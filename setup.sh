f694c8561cbddfac29318997aa91e4266fae1da1
7555G <630r63.7555@gmail.com>
2021-02-21 18:58:12 +0200

    Minor changes

--- setup.sh
+++ setup.sh
@@ -14,10 +14,10 @@ mkdir -vp ~/.config/autostart
 
 # make soft symlinks
 echo "- Symlinking dotfiles (${DOTFILES})"
-"${MACOS_DOTFILES}/bin/ln_dotfiles"        ${DOTFILES}
-ln -sfv ${MACOS_DOTFILES}/inputrc          ~/.inputrc
-ln -sfv ${MACOS_DOTFILES}/bash_aliases     ~/.bash_aliases
-ln -sfv ${MACOS_DOTFILES}/vimrc            ~/.vimrc
-ln -sfv ${MACOS_DOTFILES}/vim/*            ~/.vim
-ln -sfv ${DOTFILES}/config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
-ln -sfv ${DOTFILES}/config/autostart/*     ~/.config/autostart
+"${MACOS_DOTFILES}"/bin/ln_dotfiles        ${DOTFILES}
+ln -sfv "${MACOS_DOTFILES}"/inputrc          ~/.inputrc
+ln -sfv "${MACOS_DOTFILES}"/bash_aliases     ~/.bash_aliases
+ln -sfv "${MACOS_DOTFILES}"/vimrc            ~/.vimrc
+ln -sfv "${MACOS_DOTFILES}"/vim/*            ~/.vim
+ln -sfv "${DOTFILES}"/config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
+ln -sfv "${DOTFILES}"/config/autostart/*     ~/.config/autostart
