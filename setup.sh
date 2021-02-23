edd61061f03927103a1e25f3f1682d854b96b38b
7555G <630r63.7555@gmail.com>
2021-02-23 13:52:29 +0200

    Add bitmap font nabling

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ mkdir -vp ~/.config/autostart
 
 # make soft symlinks
 echo "- Symlinking dotfiles (${DOTFILES})"
-"${MACOS_DOTFILES}"/bin/ln_dotfiles        ${DOTFILES}
+"${MACOS_DOTFILES}"/bin/ln_dotfiles          ${DOTFILES}
 ln -sfv "${MACOS_DOTFILES}"/inputrc          ~/.inputrc
 ln -sfv "${MACOS_DOTFILES}"/bash_aliases     ~/.bash_aliases
 ln -sfv "${MACOS_DOTFILES}"/bash_prompt      ~/.bash_prompt
@@ -22,3 +22,9 @@ ln -sfv "${MACOS_DOTFILES}"/vimrc            ~/.vimrc
 ln -sfv "${MACOS_DOTFILES}"/vim/*            ~/.vim
 ln -sfv "${DOTFILES}"/config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
 ln -sfv "${DOTFILES}"/config/autostart/*     ~/.config/autostart
+ln -sfv "${DOTFILES}"/fonts/*/*.otb          ~/.local/share/fonts
+
+echo "- Enable bitmap fonts & reconfigure fontconfig"
+sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
+sudo ln -sfv /etc/fonts/conf.avail/70-yes-bitmaps.conf /etc/fonts/conf.d
+sudo dpkg-reconfigure fontconfig
