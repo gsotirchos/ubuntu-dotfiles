4e1f9c218df916e112fe6b7ab7f9f07961c6dda1
Giorgos Sotirchos <630r63.7555@gmail.com>
2021-05-24 15:27:27 +0300

    Update for split vim files

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-BRIGHT_COLOR='\033[0;97m'
-NORMAL_COLOR='\033[0m'
+BR_TEXT='\033[0;97m'
+DEF_TEXT='\033[0m'
 
 # dotfiles path
 DOTFILES=$(\
@@ -13,7 +13,7 @@ DOTFILES=$(\
 MACOS_DOTFILES=~/.macos-dotfiles
 if [[ ! -d "${MACOS_DOTFILES}" ]] \
     || [[ -z "$( ls -A ${MACOS_DOTFILES})" ]]; then
-    echo -e "${BRIGHT_COLOR}- Couldn't locate ~./macos-dotfiles folder, cloning...${NORMAL_COLOR}"
+    echo -e "${BR_TEXT}- Couldn't locate ~./macos-dotfiles folder, cloning...${DEF_TEXT}"
     git clone https://github.com/7555G/macos-dotfiles ${MACOS_DOTFILES}
     exit
 fi
@@ -27,14 +27,13 @@ mkdir -vp ~/.config/autostart
 mkdir -vp ~/.local/share/fonts
 touch ~/.hushlogin
 
-# TODO: replace this mess with a submodule & symlinks
 # make soft symlinks
-echo -e "${BRIGHT_COLOR}- Symlinking dotfiles (${DOTFILES})${NORMAL_COLOR}"
+# TODO: replace this mess with a "macos-dotfiles submodule" & symlinks
+echo -e "${BR_TEXT}- Symlinking dotfiles (${DOTFILES})${DEF_TEXT}"
 "${MACOS_DOTFILES}"/bin/ln_dotfiles "${DOTFILES}" "${HOME}/."
 ln -sfv "${MACOS_DOTFILES}"/inputrc          ~/.inputrc
 ln -sfv "${MACOS_DOTFILES}"/bash_aliases     ~/.bash_aliases
 ln -sfv "${MACOS_DOTFILES}"/bash_prompt      ~/.bash_prompt
-ln -sfv "${MACOS_DOTFILES}"/vimrc            ~/.vimrc
 ln -sfv "${MACOS_DOTFILES}"/vim/*            ~/.vim
 ln -sfv "${MACOS_DOTFILES}"/condarc          ~/.condarc
 ln -sfv "${DOTFILES}"/config/gtk-3.0/gtk.css ~/.config/gtk-3.0/gtk.css
@@ -42,25 +41,25 @@ ln -sfv "${DOTFILES}"/config/autostart/*     ~/.config/autostart
 ln -sfv "${DOTFILES}"/fonts/*/*.otb          ~/.local/share/fonts
 
 # enable bitmap fonts
-echo -e "${BRIGHT_COLOR}- Enable bitmap fonts & reconfigure fontconfig${NORMAL_COLOR}"
+echo -e "${BR_TEXT}- Enable bitmap fonts & reconfigure fontconfig${DEF_TEXT}"
 sudo rm /etc/fonts/conf.d/70-no-bitmaps.conf
 sudo ln -sfv /etc/fonts/conf.avail/70-yes-bitmaps.conf /etc/fonts/conf.d
 sudo dpkg-reconfigure fontconfig
 
 # setup vundle
 if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
-    echo -e "${BRIGHT_COLOR}- Couldn't locate ~/.vim/bundle/Vundle.vim, setting up...${NORMAL_COLOR}"
+    echo -e "${BR_TEXT}- Couldn't locate ~/.vim/bundle/Vundle.vim, setting up...${DEF_TEXT}"
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
     vim +PluginInstall +qall
 fi
 
 # setup julia
 if command -v "julia" &> /dev/null; then
-    echo -e "${BRIGHT_COLOR}- Setting up Julia${NORMAL_COLOR}"
+    echo -e "${BR_TEXT}- Setting up Julia${DEF_TEXT}"
     "${MACOS_DOTFILES}"/julia/setup-julia.sh
 fi
 
-echo -e "${BRIGHT_COLOR}- Don't forget to append the following to ~/.bashrc:
+echo -e "${BR_TEXT}- Don't forget to append the following to ~/.bashrc:
 if [[ -f ~/.bash_extra ]]; then
-    . ~/.bash_extra
-fi${NORMAL_COLOR}"
+    source ~/.bash_extra
+fi${DEF_TEXT}"
