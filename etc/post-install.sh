54c0e84746dacf245015d1f6977f345c24f7530f
7555G <630r63.7555@mgmail.com>
2021-08-15 14:21:23 +0300

    Minor typo

--- etc/post-install.sh
+++ etc/post-install.sh
@@ -53,7 +53,7 @@ echo "============================================================="
 echo " - Install some other packages"
 echo "============================================================="
 sudo apt -y update
-sudo apt -y full-upgrade
+sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y install \
     tree \
